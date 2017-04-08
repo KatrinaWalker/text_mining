30b52c92c66071656de46ba02f56e7fe98e75992
katrina <walker.katrina11@gmail.com>
2017-04-08 07:58:07 +0200

    inital python file and data added

---
+++ code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 07:51:31 2017
+
+@author: k2
+"""
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_table("speech_data_extend.txt",encoding="utf-8")
