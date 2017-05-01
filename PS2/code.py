3af8cbd19e9c5421b88ba92f8473cf25ae50a814
katrina <walker.katrina11@gmail.com>
2017-05-01 22:41:50 +0200

    code template

---
+++ PS2/code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 22:31:59 2017
+
+@author: k2
+"""
+# Question 1
+
+
+
+
+
+
+# Question 2
+import lda
+
+
+
+
+
+
+
+# Question 3
